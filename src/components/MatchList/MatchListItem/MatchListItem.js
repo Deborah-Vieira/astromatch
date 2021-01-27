d3c691db666830b61d00688fd952192d0285cf8e
Deborah Vieira <deborahanalistati@gmail.com>
2021-01-27 18:16:07 -0300

    Screen-Match

---
+++ src/components/MatchList/MatchListItem/MatchListItem.js
@@ -0,0 +1,32 @@
+
+import React from 'react';
+import styled from 'styled-components';
+
+
+const ListItemContainer = styled.div `
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #cbc;
+
+  :hover{
+    background-color: #e7505e;
+  }`
+
+ const Avatar =  styled.img ` 
+  border-radius: 50%;
+  margin-right: 8px;
+  width: 40px;
+  height: 40px;
+ ` 
+
+function MatchListItem(props) {
+  const profile = props.profile
+    return <div>
+    <ListItemContainer>
+      <Avatar src={profile.photo} />
+      <p>{profile.name}</p>
+    </ListItemContainer>    
+  </div>;
+}
+
+export default MatchListItem;
